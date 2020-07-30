1c0ce1fafebd88333a33eb905dcbf3c7249a0595
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-30 18:32:22 +0800

    Added user name in dashboard.

--- src/screens/drawers/Dashboard.tsx
+++ src/screens/drawers/Dashboard.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components/native';
 import { TouchableWithoutFeedback, Keyboard, StyleSheet } from 'react-native';
 import { Layout, Input, Text, Button, Toggle } from '@ui-kitten/components';
+import { userContext } from '../../models';
 
 const Splash: React.FC = () => {
-  return <Layout style={styles.container} />;
+  const userCtx = useContext(userContext);
+  return (
+    <Layout style={styles.container}>
+      <Text category="h1">
+        Hello {`${userCtx.user.firstName} ${userCtx.user.lastName}`}
+      </Text>
+    </Layout>
+  );
 };
 
 const styles = StyleSheet.create({
