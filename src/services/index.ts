d0f84d493b2c64fd6f1f70ed1996c91761eedd5f
Paul Catalan <24451518+polcats@users.noreply.github.com>
2020-07-30 19:31:59 +0800

    Updated API URL.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,7 @@
 import ky from 'ky';
 
-const ApiUrl = 'https://www.placeholder.com/api';
+// since dotenv and process its not working
+const ApiUrl = process.env.REACT_APP_API_URL || 'http://192.168.100.24:8000';
 const api = ky.create({ prefixUrl: ApiUrl });
 
 export default api;
